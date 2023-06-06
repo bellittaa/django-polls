35c09274d9300e1006eb84ea63b333b41586cefc
isabella <isabella.conta.ifsp@gmail.com>
2023-06-06 11:26:22 -0300

    aula 06/06

--- polls/views.py
+++ polls/views.py
@@ -2,4 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Olá Mundo!")
+    return HttpResponse("Página inicial")
+
+def detail(request, question_id):
+    return HttpResponse(f"Essa é a pergunta de número {question_id}")
+
+def results(request, question_id):
+     return HttpResponse (f"Resultados da pergunta de número {question_id}")
+
+def vote(request, question_id):
+    return HttpResponse(f"Você vai votar na pergunta de numero {question_id}")
+
+
+
