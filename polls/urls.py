d479264079081871dcf29c272b5e8fafea780082
Isabella Vitoria <isabella.conta.ifsp@gmail.com>
2023-08-30 06:33:45 -0300

    30/08

--- polls/urls.py
+++ polls/urls.py
@@ -4,14 +4,16 @@ from polls.models import Question
 from . import views
 urlpatterns = [
     # function based viws
-    path("", views.index, name="index"),
-    path("<int:question_id>/", views.detail, name="detail"),
-    path("<int:question_id>/results/", views.results, name="results"),
-    path("<int:question_id>/vote/", views.vote, name="vote"),
+    # path("", views.index, name="index"),
+    # path("<int:question_id>/", views.detail, name="detail"),
+    # path("<int:question_id>/results/", views.results, name="results"),
+    # path("<int:question_id>/vote/", views.vote, name="vote"),
 
     # class based views 
     path('listar', views.QuestionListView.as_view(),name = "question-list"),
-    path('cadastrar', views.QuestionCreateView.as_view(),name="question-create")
+    path('cadastrar', views.QuestionCreateView.as_view(),name="question-create"),
+    path('<int:pk>', views.QuestionDetailView.as_view(), name="question-detail")
+
     
 
 ]
